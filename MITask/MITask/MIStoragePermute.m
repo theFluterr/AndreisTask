5183030ccf284c7a0dfd0597af97f1df7d955e12
theFluterr <f.l.i.t.e1.9.9.6@gmail.com>
2016-03-14 21:13:36 +0200

    Add comments

--- MITask/MITask/MIStoragePermute.m
+++ MITask/MITask/MIStoragePermute.m
@@ -10,6 +10,7 @@
 
 @implementation MIStoragePermute
 
+//Initialize the object with two arrays
 -(id)init {
     self = [super init];
     if (!self) return nil;
@@ -19,6 +20,7 @@
     return self;
 }
 
+//Prepare the output by mapping alphabetic array to corresponding indexes in numeric array (the array which was permutated)
 -(NSArray*)returnAlphabeticArray {
     for (NSNumber *number in storageNumberArray)
         [finalOutput addObject:[storageAlphabeticArray objectAtIndex:[number intValue]]];
@@ -27,6 +29,7 @@
     return finalOutput;
 }
 
+//Find the "second" integer
 -(int)findSecondIntegerIndex:(NSArray*)array forFirstInteger:(int)firstInteger afterIndex:(int)index andLastIndex:(int)lastIndex{
     int secondIndex = index;
     for (int i = index + 1; i <= lastIndex; i++)
@@ -35,18 +38,26 @@
     return secondIndex;
 }
 
--(void)swapFirstInteger:(int)first andSecond:(int)second andSortAfterIndex:(int)firstIndex  withSecondIndex:(int)secondIndex{
-
+//Swap "first" int with "second", then sort the rest of array
+-(void)swapFirstInteger:(int)first withSecond:(int)second andSortAfterIndex:(int)firstIndex  withSecondIndex:(int)secondIndex{
     NSMutableArray *mutableCopy = [[NSMutableArray alloc] initWithArray:storageNumberArray];
+    //Swap
     [mutableCopy replaceObjectAtIndex:firstIndex withObject:[NSNumber numberWithInt:second]];
     [mutableCopy replaceObjectAtIndex:secondIndex withObject:[NSNumber numberWithInt:first]];
-    
+    //Sort
     NSArray *sortingArray = [mutableCopy objectsAtIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(firstIndex + 1, [storageNumberArray count] - 1 - firstIndex)]];
     sortingArray = [sortingArray sortedArrayUsingSelector:@selector(compare:)];
     [mutableCopy replaceObjectsAtIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(firstIndex + 1, [storageNumberArray count] -1 - firstIndex)] withObjects:sortingArray];
     
     storageNumberArray = (NSArray *)mutableCopy;
 }
+
+//Permutation function
+//Firstly, find the "rightmost" int that is smaller than its neighbour on the right (the "first" int)
+//Then, find the int on the right hand after the "first" integer, but is still the smallest one (the "second" int)
+//Swap two integers
+//Sort the part of the array that is on the right of the "first" int previous position
+//Output, if reached the required permutation
 
 -(void)permuteArrayOfNumbers:(int)times {
     int first = 0;
@@ -56,8 +67,7 @@
     BOOL isPermuted = false;
     int i = 0;
     while (isPermuted == NO) {
-        
-        //NSLog(@"%@", [self returnAlphabeticArray]);
+        //Find the "first" int and its index
         iterationNumber++;
         for (i = (int)[storageNumberArray count] - 2; i >= 0; --i) {
             if ([storageNumberArray objectAtIndex:i] < [storageNumberArray objectAtIndex:i+1]){
@@ -66,18 +76,19 @@
                 break;
             }
         }
-        
-        if (iterationNumber == times){
-            isPermuted = YES;
+        //Check for completion & output
+        if (iterationNumber == times || i == -1){
             NSLog(@"%@", [self returnAlphabeticArray]);
+            isPermuted = YES;
         }
     
         else {
-            
+            //Find the "second" int and its index
             int secondIndex = [self findSecondIntegerIndex:storageNumberArray forFirstInteger:first afterIndex:firstIndex + 1 andLastIndex:(int)[storageNumberArray count] - 1];
             second = [[storageNumberArray objectAtIndex:secondIndex] intValue];
             
-            [self swapFirstInteger:first andSecond:second andSortAfterIndex:firstIndex withSecondIndex:secondIndex];
+            //Swap & sort
+            [self swapFirstInteger:first withSecond:second andSortAfterIndex:firstIndex withSecondIndex:secondIndex];
         }
     }
 }
